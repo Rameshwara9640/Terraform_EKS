132d7f6b58107b871fcb705e924399fdc8f31adc
Rameshwara Reddy <149377379+Rameshwara9640@users.noreply.github.com>
2026-06-21 10:56:55 +0530

    Fix typo in S3 bucket name

--- backend/main.tf
+++ backend/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "demo-eks-statefile-s3-jerneybucket"
+  bucket = "demo-eks-statefile-s3-jerney-bucket"
 
   lifecycle {
     prevent_destroy = false
